63c31437b2b1c1491e95d720d684c060a2d67b12
guicruz12 <130380751+guicruz12@users.noreply.github.com>
2024-10-11 20:18:46 -0300

    Nodejs basico

---
+++ NodejsBasico01js/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+console.dir(http);
+
+/*
+const arr = [1, 23, 32, 45, 54, 6, 10]
+
+console.log(arr)
+console.table(arr)
+console.dir(arr)
+console.error(arr)
+console.info(arr)
+console.warn(arr)
+console.time(arr)
+
+*/
+s
